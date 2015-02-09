6801d6e13df914093cb899e9b8d328b15d4b21d0
rchavarria <txingo@gmail.com>
2015-02-09 23:03:36 +0100

    Add controller and button to play a sound from the internet

--- playSound/www/js/app.js
+++ playSound/www/js/app.js
@@ -12,3 +12,19 @@ angular.module('playSound', ['ionic', 'ngCordova'])
     }
   });
 })
+
+.controller('ExampleController', function($scope, $cordovaMedia, $ionicLoading) {
+
+    $scope.play = function (src) {
+        var media = new Media(src, null, null, mediaStatusCallback);
+        $cordovaMedia.play(media);
+    };
+ 
+    var mediaStatusCallback = function(status) {
+        if(status == 1) {
+            $ionicLoading.show({template: 'Loading...'});
+        } else {
+            $ionicLoading.hide();
+        }
+    };
+});
